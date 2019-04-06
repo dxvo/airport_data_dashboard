9ec7c9174b1e0533a0ac6afb0501ca12917dda13
dxvo <dxvo@uh.edu>
2019-04-05 21:28:21 -0500

    update

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-#app.config["CLEARDB_DATABASE_URL"] =  configs.database_link
+app.config["CLEARDB_DATABASE_URL"] =  configs.database_link
+#
 db = SQLAlchemy(app)
 
 # reflect an existing database into a new model
